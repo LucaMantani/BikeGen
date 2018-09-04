c405a03090e5c968cb069aacf617e6976de2b7ab
Marius Utheim <marius_utheim@hotmail.com>
2018-09-04 17:40:27 +0200

    add Visualiser.py

---
+++ Visualiser.py
@@ -0,0 +1,31 @@
+import numpy as np
+from matplotlib.patches import Circle
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class Visualiser:
+
+    def run(self):
+        
+        figure = plt.figure()
+        axes = figure.add_subplot('111',aspect='equal')
+        axes.set_xlim(-2,7)
+        axes.set_ylim(-2,7)
+
+        # add a patch to the axis
+        ball = Circle((0.1,0.1), radius=2)
+        axes.add_patch(ball)
+
+        def animate(i):
+            # shift the ball's position
+            ball.center = (i/10.,i/15.)
+            return ball,
+
+        # afterwards, switch to zoomable GUI mode
+        ani = animation.FuncAnimation(figure, 
+                                    animate, 
+                                    np.arange(1, 50), 
+                                    interval=25,
+                                    blit=True)
+
+        plt.show()
