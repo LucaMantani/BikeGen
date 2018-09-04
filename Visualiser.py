5c069973fdbbfb3b2fc2beec7802a10c1854aa25
Luca Mantani <luca@MacBook-Pro-di-Luca.local>
2018-09-04 17:54:27 +0200

    Cleaning Visualiser class

--- Visualiser.py
+++ Visualiser.py
@@ -3,17 +3,18 @@ from matplotlib.patches import Circle
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
 class Visualiser:
 
     def run(self):
-        
+
         figure = plt.figure()
-        axes = figure.add_subplot('111',aspect='equal')
-        axes.set_xlim(-2,7)
-        axes.set_ylim(-2,7)
+        axes = figure.add_subplot('111', aspect='equal')
+        axes.set_xlim(-2, 7)
+        axes.set_ylim(-2, 7)
 
         # add a patch to the axis
-        ball = Circle((0.1,0.1), radius=2)
+        ball = Circle((0.1, 0.1), radius=2)
         axes.add_patch(ball)
 
         def init():
@@ -22,16 +23,16 @@ class Visualiser:
 
         def animate(i):
             # shift the ball's position
-            if i == 1: ball.set_visible(True)
-            ball.center = (i/10.,i/15.)
+            if i == 1 : ball.set_visible(True)
+            ball.center = (i/10., i/15.)
             return ball,
 
         # afterwards, switch to zoomable GUI mode
-        ani = animation.FuncAnimation(figure, 
-                                    animate, 
-                                    np.arange(1, 50), 
-                                    init_func = init,
-                                    interval=25,
-                                    blit=True)
+        ani = animation.FuncAnimation(figure,
+                                      animate,
+                                      np.arange(1, 50),
+                                      init_func=init,
+                                      interval=25,
+                                      blit=True)
 
         plt.show()
