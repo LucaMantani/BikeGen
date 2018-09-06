736d2118fee7fe2bf9a80451194b727722f01596
Marius Utheim <marius_utheim@hotmail.com>
2018-09-06 17:59:07 +0200

    create copies of bikes before simulating them

--- SimTest.py
+++ SimTest.py
@@ -4,16 +4,17 @@ from Visualiser import Visualiser
 import Genetics
 from Ground import Ground
 import Physics
-
+import copy
 
 
 theBike = Genetics.generateBike(0.25)
-bikes = Genetics.generateInitialPopulation(0.25, 100)
+bikes = Genetics.generateInitialPopulation(0.25, 10)
 maxlifetime = 100
 
 theGround = Ground([ Vector(-1, -1), Vector(-1, -1), Vector(-1, -1), Vector(-1, -1) ])
 
 def simulate(bike):
+    bike = copy.deepcopy(bike)
     for i in range(1,maxlifetime):
         if not Physics.timestep(bike):
             #record the distance travelled and then break
@@ -31,7 +32,6 @@ def updateVertices(frameNumber):
     if frameNumber % maxlifetime == 0:
         bikes = Genetics.evolve(bikes,simulate)
         theBike.wheels = bikes[0].wheels
-        print(simulate(bikes[1]))
     else:
         Physics.timestep(theBike)
 
