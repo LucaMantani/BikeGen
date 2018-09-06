6f753db7bc79f0fd02f2e2c37f791128a9769bfa
Kevin Kwok <ka.w.kwok@durham.ac.uk>
2018-09-06 22:17:26 +0200

    Visualiser extend range

--- Visualiser.py
+++ Visualiser.py
@@ -17,7 +17,7 @@ class Visualiser:
         # Initialize figure
         figure = plt.figure()
         axes = figure.add_subplot('111', aspect='equal')
-        axes.set_xlim(-1, 1)
+        axes.set_xlim(-1, 5)
         axes.set_ylim(-1, 1)
 
         # Initialize graphics objects
