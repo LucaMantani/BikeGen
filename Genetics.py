cef653be17ca77df1788089bb38dfa610c93e693
Marius Utheim <marius_utheim@hotmail.com>
2018-09-06 15:02:46 +0200

    update Bike

--- Genetics.py
+++ Genetics.py
@@ -1,6 +1,6 @@
 import numpy as np
-import numpy.random as rand
-from Bike import Bike
+import numpy.random
+from Bike import Wheel, Spring, Bike
 from Vector import Vector
 
 wheelDistanceExtension = 0.1
@@ -8,41 +8,54 @@ mutationRate = 0.1
 mutationAmount = 0.25
 
 def generateWheel(sigma):
-    phi = rand.uniform(0, 2 * np.pi)
-    r = rand.normal(0, sigma)
-    return Vector(r * np.cos(phi), r * np.sin(phi))
+    phi = np.random.uniform(0, 2 * np.pi)
+    r = np.random.normal(0, sigma)
+    return Wheel(Vector(r * np.cos(phi), r * np.sin(phi)), 1, 0.1) 
 
 def generateBike(sigma):
-    return Bike([ generateWheel(sigma) for _ in range(4) ])
+    return Bike(
+        [ generateWheel(sigma) for _ in range(4) ],
+        [
+            Spring(0, 1, 0.01, 0.01),
+            Spring(0, 2, 0.01, 0.01),
+            Spring(0, 3, 0.01, 0.01),
+            Spring(1, 2, 0.01, 0.01),
+            Spring(1, 3, 0.01, 0.01),
+            Spring(2, 3, 0.01, 0.01)
+        ])
 
 def generateInitialPopulation(sigma, nIndividuals):
     return [ generateBike(sigma) for _ in range(nIndividuals) ]
 
 def fitness(bike):
     invFitness = sum([ 
-        ((v1 - v2).r - 1) ** 2 for (v1, v2) in bike.springs()
+        ((bike.vertices[s.i1] - bike.vertices[s.i2]).r - 1) ** 2 
+        for s in bike.springs
     ])
     return 1.0 / invFitness
 
 
 def crossoverWheel(w1, w2):
     t = np.random.uniform(-wheelDistanceExtension, 1.0 + wheelDistanceExtension)
-    return (1 - t) * w1 + t * w2
+    vertex = (1 - t) * w1.pos + t * w2.pos
+    # TODO: Evolve mass and radius if relevant
+    return Wheel(vertex, w1.mass, w1.radius)
 
 def mutate(child):
-    for i in range(len(child.vertices)):
+    for wheel in child.wheels:
         if np.random.rand() < mutationRate:
             phi = np.random.rand() * 2 * np.pi
             r = np.random.normal(0, mutationAmount)
-            child.vertices[i] += Vector(r * np.cos(phi), r * np.sin(phi))
+            wheel.pos += Vector(r * np.cos(phi), r * np.sin(phi))
     
 
 def generateOffspring(parents):
-    newWheels = []
-    for i in range(len(parents[0].vertices)):
-        newWheels.append(crossoverWheel(parents[0].vertices[i], parents[1].vertices[i]))
-    
-    child = Bike(newWheels)
+
+    newWheels = [ crossoverWheel(w1, w2) 
+                  for (w1, w2) in zip(parents[0].wheels, parents[1].wheels) ]
+
+    # TODO crossover springs
+    child = Bike(newWheels, parents[0].springs)
     mutate(child)
     return child
 
