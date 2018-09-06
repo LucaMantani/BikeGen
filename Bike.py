cbed64c8e87db55ce7bffe600df51868d6f59a6c
Marius Utheim <marius_utheim@hotmail.com>
2018-09-06 09:39:28 +0200

    make Bike constructor take a list of springs

--- Bike.py
+++ Bike.py
@@ -12,20 +12,13 @@ class Bike:
 
     idxs = [ (0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3) ]
 
-    def __init__(self, vertices, springConstants, springDampings, masses, radii):
+    def __init__(self, vertices, springs, masses, radii):
         self.vertices = vertices
         self.velocities = [ Vector(0, 0) for _ in vertices ]
-        self.springs = [ Spring(Bike.idxs[i][0], Bike.idxs[i][1], springConstants[i], springDampings[i])
-                         for i in range(len(springConstants)) ]
+        self.springs = springs
         self.masses = masses
         self.radii = radii
 
     def springVertices(self):
-        return [ 
-            (self.vertices[0], self.vertices[1]), 
-            (self.vertices[1], self.vertices[2]), 
-            (self.vertices[2], self.vertices[3]), 
-            (self.vertices[3], self.vertices[0]), 
-            (self.vertices[0], self.vertices[2]), 
-            (self.vertices[1], self.vertices[3])
-        ]
+        return [ (self.vertices[s.i1], self.vertices[s.i2]) for s in self.springs ]
+        
--- PhysicsTest.py
+++ PhysicsTest.py
@@ -1,11 +1,17 @@
-from Bike import Bike
+from Bike import Bike, Spring
 from Vector import Vector
 from Visualiser import Visualiser
 import Physics
 
 theBike = Bike([ Vector(-0.4, -0.4), Vector(0.8, -0.9), Vector(0.55, 0.5), Vector(-0.45, 0.45) ],
-               [ 0.01, 0.01, 0.01, 0.01, 0.1, 0.001 ],
-               [0.01, 0.01, 0.01, 0.01, 0.001, 0.001],
+               [
+                   Spring(0, 1, 0.01, 0.01),
+                   Spring(0, 2, 0.01, 0.01),
+                   Spring(0, 3, 0.01, 0.01),
+                   Spring(1, 2, 0.01, 0.01),
+                   Spring(1, 3, 0.01, 0.001),
+                   Spring(2, 3, 0.001, 0.001)
+               ],
                [1, 1, 10, 10],
                [ 0.05, 0.05, 0.01, 0.01 ])
 
