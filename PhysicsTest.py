559cd2bc7f801461b5bcf785ba714e72e85061d7
Luca Mantani <luca@MacBook-Pro-di-Luca.local>
2018-09-06 17:37:14 +0200

    Nice PhysicsTest.py

--- PhysicsTest.py
+++ PhysicsTest.py
@@ -5,21 +5,21 @@ from Visualiser import Visualiser
 import Physics
 
 theBike = Bike([ 
-                    Wheel(Vector(-0.4, -0.4), 1, 0.05),
-                    Wheel(Vector(-0.3, -0.5), 1, 0.05),
-                    Wheel(Vector(0.55, 0.5), 10, 0.01),
-                    Wheel(Vector(-0.45, 0.45), 10, 0.01)
+                    Wheel(Vector(-0.4, -0.3), 1, 0.03),
+                    Wheel(Vector(-0.45, -0.3), 1, 0.03),
+                    Wheel(Vector(-0.4, -0.25), 1, 0.01),
+                    Wheel(Vector(-0.45, -0.25), 1, 0.01)
                ],
                [
-                    Spring(0, 1, 0.01, 0.01),
-                    Spring(0, 2, 0.01, 0.01),
-                    Spring(0, 3, 0.01, 0.01),
-                    Spring(1, 2, 0.01, 0.01),
-                    Spring(1, 3, 0.1, 0.001),
-                    Spring(2, 3, 0.001, 0.001)
+                    Spring(0, 1, 0.1, 0.01),
+                    Spring(0, 2, 0.1, 0.01),
+                    Spring(0, 3, 0.1, 0.01),
+                    Spring(1, 2, 0.1, 0.01),
+                    Spring(1, 3, 0.1, 0.01),
+                    Spring(2, 3, 0.1, 0.01)
                ])
 
-ground = Ground([ Vector(-1, -1), Vector(-0.25, -0.5), Vector(0.25, -0.75), Vector(1, -0.8) ])
+ground = Ground([ Vector(-1, -0.5), Vector(-0.75, -0.75), Vector(-0.25, -0.5), Vector(0.25, -0.75), Vector(0.5, -0.8), Vector(1, -0.5) ])
 
 def updateBike(frameNumber):
     Physics.timestep(theBike, ground)
