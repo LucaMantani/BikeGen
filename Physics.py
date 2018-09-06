d1668e9b34accaa17a61020bfa4f5fd6819d4b54
Marius Utheim <marius_utheim@hotmail.com>
2018-09-06 10:21:00 +0200

    use for wheel in bike.wheels whereever applicable

--- Physics.py
+++ Physics.py
@@ -9,15 +9,15 @@ g_N = 0.0005
 driving = 0.001
 
 def springs(bike):
-    forces = [ Vector(0, 0) for _ in bike.vertices ]
+    forces = [ Vector(0, 0) for _ in bike.wheels ]
 
     for s in bike.springs:
-        d = bike.vertices[s.i2] - bike.vertices[s.i1]
+        w1, w2 = bike.wheels[s.i1], bike.wheels[s.i2]
+        d = w2.pos - w1.pos
 
         F = s.k * (d.r - springx0) * d.unitVector()
-
-        forces[s.i1] += F - s.damping * bike.wheels[s.i1].vel
-        forces[s.i2] -= F + s.damping * bike.wheels[s.i2].vel
+        forces[s.i1] += F - s.damping * w1.vel
+        forces[s.i2] -= F + s.damping * w2.vel
 
     for i in range(len(forces)):
         bike.wheels[i].vel += forces[i] / bike.wheels[i].mass
@@ -26,26 +26,27 @@ def springs(bike):
 def gravity(bike):
     gravity_force = Vector(0.0, -g_N)
 
-    for i in range(len(bike.vertices)):
-        bike.wheels[i].vel += gravity_force
+    for wheel in bike.wheels:
+        wheel.vel += gravity_force
+
 
 def ground_collision(bike):
-    for i in range(len(bike.vertices)):
-        if (bike.vertices[i].y - bike.wheels[i].radius) < -1.0:
-            bike.wheels[i].pos = Vector(bike.vertices[i].x, -1.0 + bike.wheels[i].radius)
-            bike.wheels[i].vel = Vector(bike.wheels[i].vel.x, 0.0)
+    for wheel in bike.wheels:
+        if (wheel.pos.y - wheel.radius) < -1.0:
+            wheel.pos = Vector(wheel.pos.x, -1.0 + wheel.radius)
+            wheel.vel = Vector(wheel.vel.x, 0.0)
 
 
 def timestep(bike):
     springs(bike)
     gravity(bike)
 
-    for i in range(len(bike.vertices)):
-        bike.wheels[i].pos += bike.wheels[i].vel
+    for wheel in bike.wheels:
+        wheel.pos += wheel.vel
 
     ground_collision(bike)
 
-    if bike.vertices[0].y == -1.0 + bike.wheels[i].radius:
+    if bike.vertices[0].y == -1.0 + bike.wheels[0].radius:
         bike.wheels[0].vel += driving / bike.wheels[0].mass * Vector(1.0, 0.0)
 
 
