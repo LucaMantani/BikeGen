23fdcd5e9b9f8f36ac775f133bff9d1090047c84
Kevin Kwok <ka.w.kwok@durham.ac.uk>
2018-09-06 15:26:56 +0200

    Added isFragile to the wheels and attempted to combine everything for the simulator

--- SimTest.py
+++ SimTest.py
@@ -2,12 +2,15 @@ from Bike import Bike
 from Vector import Vector
 from Visualiser import Visualiser
 from Genetics import Genetics
+from Ground import Ground
+from Physics import Physics
 
 
 
 theBike = Genetics.generateBike(0.5)
 bikes = Genetics.generateInitialPopulation(0.5, 100)
 
+theGround = Ground([ Vector(-1, -1), Vector(-0.25, -0.5), Vector(0.25, -0.75), Vector(1, -0.8) ])
 
 def updateVertices(frameNumber):
     global bikes
@@ -16,5 +19,8 @@ def updateVertices(frameNumber):
         theBike.wheels = bikes[0].wheels
     
 
-v = Visualiser(theBike, updateVertices)
+def updateBike(frameNumber):
+    Physics.timestep(theBike)
+
+v = Visualiser(theBike, theGround, updateBike)
 v.run()
